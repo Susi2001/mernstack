db064f21d7bcdb3b1fa1f9657a6a012561d2479a
susi2001 <rajsusi568@gmail.com>
2023-04-21 17:23:34 +0530

    read

---
+++ txt.js
@@ -0,0 +1,17 @@
+onst name = "sherlock";
+console.log(name);
+const greet = (name) => {
+console.log('hello,${name}');
+}
+greet('bahubali');
+greet('kattappa');
+global.setTimeout(() =>{
+    console.log('in the timeout');
+},5000);
+const int = setInterval(() =>{
+    console.log('In the every interval');
+},2000);
+setTimeout(() =>{
+    console.log('In the timeout');
+    clearInterval(int)
+},5000);
